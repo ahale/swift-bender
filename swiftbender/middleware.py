0aeafbdf56ffdc86478434af0cd1a23f44df0a96
Andrew Hale <andy@wwwdata.eu>
2012-05-09 09:23:18 +0000

    update

--- swiftbender/middleware.py
+++ swiftbender/middleware.py
@@ -10,7 +10,6 @@ class Bender(object):
     def __call__(self, env, start_response):
         def _start_response(status, headers, exc_info=None):
             quote = benderism[random.randrange(0,len(benderism))]
-            #headers = dict(headers).items()
             headers.append(('X-Bender', quote))
             return start_response(status, headers, exc_info)
         return self.app(env, _start_response)
